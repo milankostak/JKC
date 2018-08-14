14bbd4bcdb0049cce835aa63da9319d8acc437db
Milan Košťák <milankostak@seznam.cz>
2018-08-14 10:34:02 +0200

    typo

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -44,7 +44,7 @@ abstract class BasePresenter extends Presenter {
 				case 5: return "Květen";
 				case 6: return "Červen";
 				case 7: return "Červenec";
-				case 8: return "Spren";
+				case 8: return "Srpen";
 				case 9: return "Září";
 				case 10: return "Říjen";
 				case 11: return "Listopad";
